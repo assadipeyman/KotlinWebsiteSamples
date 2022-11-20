fb8a53e012ff00a1dfa8f05ca62257555a43ab8e
p.asadi <p.asadi@asanpardakht.ir>
2022-11-20 10:39:12 +0330

    Loop

---
+++ src/main/kotlin/Loops/IterateOverRange.kt
@@ -0,0 +1,33 @@
+fun main(){
+    for (i in 1..10) { print("$i ") }  // closed range: includes 100
+    println()
+
+    for (i in 1 until 10) {  print("$i ") } // half-open range: does not include 100
+    println()
+
+    for (x in 1..10 step 2) {  print("$x ") }
+    println()
+
+    for (x in 2..10 step 2) {  print("$x ") }
+    println()
+
+    for (x in 10 downTo 1) {  print("$x ") }
+    println()
+
+    for (x in 10 downTo 1 step 2) {  print("$x ") }
+    println()
+
+    (1..10).forEach {  print(it) }
+
+    println()
+
+
+    for(char in 'a' .. 'z')
+        print(char)
+
+    println()
+
+    for(char in 'z' downTo 'a' step 2)
+        print(char)
+
+}
